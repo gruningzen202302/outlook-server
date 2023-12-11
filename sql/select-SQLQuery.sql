5a8afef499937d17c13dd6b9d01ccb2b2f019375
vir-air-vent <alberto.gruning.zen@icloud.com>
2023-12-11 01:05:51 -0300

    update queries for new tables

--- sql/select-SQLQuery.sql
+++ sql/select-SQLQuery.sql
@@ -1,1 +1,16 @@
+/* 
+insert into dbo.timelog (title, description, hours)
+values('udemy excercises','Quizes and overall practice',4)
+*/
+
 select * from timelog
+
+select * from dbo.Permissions
+
+--delete from dbo.PermissionTypes where Id > 3
+-- INSERT INTO [PermissionTypes] ([Description])
+-- VALUES (N'Permision used by default when no other type is specified'),
+-- (N'Permission used to access the coworking area'),
+-- (N'Permission used to access the meeting room');
+
+select * from dbo.PermissionTypes
